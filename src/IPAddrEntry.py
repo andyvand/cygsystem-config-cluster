0bc8b0b63362b86a3849c48f8940fa39d55a4d57
Jim Parsons <jparsons@redhat.com>
2005-03-10 16:07:35 +0000

    Genesis: generic ip address entry widget

---
+++ src/IPAddrEntry.py
@@ -0,0 +1,67 @@
+import gtk
+
+class IP (gtk.HBox):
+  def __init__ (self):
+    gtk.HBox.__init__ (self)
+    self.set_spacing (3)
+
+    self.e1 = gtk.Entry ()
+    self.e1.set_property ("xalign", 1.0)
+    self.e1.set_property ("width-chars", 3)
+    self.e1.set_property ("max_length", 3)
+    self.pack_start (self.e1, False, False, 0)
+    self.pack_start (gtk.Label ('-'), False, False, 0)
+
+    self.e2 = gtk.Entry ()
+    self.e2.set_property ("xalign", 1.0)
+    self.e2.set_property ("width-chars", 3)
+    self.e2.set_property ("max_length", 3)
+    self.pack_start (self.e2, False, False, 0)
+    self.pack_start (gtk.Label ('-'), False, False, 0)
+
+    self.e3 = gtk.Entry ()
+    self.e3.set_property ("xalign", 1.0)
+    self.e3.set_property ("width-chars", 3)
+    self.e3.set_property ("max_length", 3)
+    self.pack_start (self.e3, False, False, 0)
+    self.pack_start (gtk.Label ('-'), False, False, 0)
+
+    self.e4 = gtk.Entry ()
+    self.e4.set_property ("xalign", 1.0)
+    self.e4.set_property ("width-chars", 3)
+    self.e4.set_property ("max_length", 3)
+    self.pack_start (self.e4, False, False, 0)
+
+  def clear(self):
+    self.e1.set_text("")
+    self.e2.set_text("")
+    self.e3.set_text("")
+    self.e4.set_text("")
+
+  def getAddrAsString(self):
+    rtval = self.e1.get_text() + "." + \
+            self.e2.get_text() + "." + \
+            self.e3.get_text() + "." + \
+            self.e4.get_text()
+
+    return rtval
+
+  def getAddrAsList(self):
+    rtlist = list()
+    rtlist.append(self.e1.get_text())
+    rtlist.append(self.e2.get_text())
+    rtlist.append(self.e3.get_text())
+    rtlist.append(self.e4.get_text())
+
+    return rtlist
+
+  def setAddrFromString(self, addr):
+    octets = addr.split(".")
+    self.e1.set_text(octets[0])
+    self.e2.set_text(octets[1])
+    self.e3.set_text(octets[2])
+    self.e4.set_text(octets[3])
+
+  def setAddrFromList(self, addr):
+    pass
+
