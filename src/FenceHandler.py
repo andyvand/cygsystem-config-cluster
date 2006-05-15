c7be95a07468024e23c776bef58666fc685c85f5
Ryan McCabe <rmccabe@redhat.com>
2006-05-15 19:26:18 +0000

    *** empty log message ***

--- src/FenceHandler.py
+++ src/FenceHandler.py
@@ -135,6 +135,7 @@ class FenceHandler:
 
     if not enable_apc_snmp:
         del FENCE_OPTS['fence_apc_snmp']
+        del PRETTY_NAME_ATTRS['fence_apc_snmp']
         del FENCE_FD_ATTRS['fence_apc_snmp']
         del FENCE_FI_ATTRS['fence_apc_snmp']
 
